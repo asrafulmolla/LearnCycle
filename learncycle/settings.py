d45e09a936e19e2095e33f3d617eceb37cbbea03
Md. Asraful Molla <armdasraful@gmail.com>
2025-10-22 13:41:52 +0600

    hosts to ready

--- learncycle/settings.py
+++ learncycle/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-u*wkc6*ww_j7h=x76tp_9w0a4mdh@mfiy)b*e1vrpuex#+8i+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
